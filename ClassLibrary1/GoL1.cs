0a272b52d2ff8ae7e659bc07960752de0140e37a
Zeno Foltin <zeno.foltin@gmail.com>
2012-09-30 22:07:35 +0100

    Board and Position implementation, refactorings

---
+++ ClassLibrary1/GoL1.cs
@@ -0,0 +1,163 @@
+﻿namespace ClassLibrary1
+{
+    using NUnit.Framework;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    [TestFixture]
+    public class GoL1
+    {
+        [TestCase(0)]
+        [TestCase(1)]
+        public void LiveCellWithLessThanTwoNeighbourShouldDie(int noOfNeighbours)
+        {
+            bool nextState = Cell.IsAlive(noOfNeighbours);
+
+            Assert.That(nextState, Is.False);
+        }
+
+        [TestCase(2)]
+        [TestCase(3)]
+        public void LiveCellWithTwoOrThreeNeighbourShouldLive(int noOfNeighbours)
+        {
+            bool nextState = Cell.IsAlive(noOfNeighbours);
+
+            Assert.That(nextState, Is.True);
+        }
+
+        [TestCase(4)]
+        [TestCase(5)]
+        [TestCase(6)]
+        [TestCase(7)]
+        [TestCase(8)]
+        public void LiveCellWithMoreThanThreeNighboursShouldDie(int noOfNeighbours)
+        {
+            bool nextState = Cell.IsAlive(noOfNeighbours);
+
+            Assert.That(nextState, Is.False);
+        }
+
+        [Test]
+        public void DeadCellWithThreeNeighboursShouldBecomeLive()
+        {
+            bool nextState = Cell.IsAlive(3);
+
+            Assert.That(nextState, Is.True);
+        }
+
+        [Test]
+        public void EmptyBoardShouldReturnEmptyNextTick()
+        {
+            Position[] newLiveCellsPositions = Board.Tick(null);
+            Assert.That(newLiveCellsPositions, Is.Not.Null);
+
+        }
+
+        [Test]
+        public void OneLiveCellOnBoardDiesInNextTick()
+        {
+            Position[] positions = Board.Tick(new[] { new Position(5, 5) });
+
+            Assert.AreEqual(0, positions.Length);
+        }
+
+        [Test]
+        public void ThreeAdjacentCellsOnlyMiddleCellStaysAlive()
+        {
+            Position[] positions = Board.Tick(new[]
+                {
+                    new Position(4, 5),
+                    new Position(5, 5), 
+                    new Position(6, 5)
+                });
+
+            CollectionAssert.DoesNotContain(positions, new Position(4, 5));
+            CollectionAssert.DoesNotContain(positions, new Position(6, 5));
+            CollectionAssert.Contains(positions, new Position(5, 5));
+        }
+
+        [Test]
+        public void ThreeAdjacentHorizontalCellsSpawnTwoNewCells()
+        {
+            Position[] positions = Board.Tick(new[]
+                {
+                    new Position(4, 5),
+                    new Position(5, 5), 
+                    new Position(6, 5)
+                });
+
+            CollectionAssert.Contains(positions, new Position(5, 5));
+            CollectionAssert.Contains(positions, new Position(5, 4));
+            CollectionAssert.Contains(positions, new Position(5, 6));
+        }
+    }
+
+    public class Position
+    {
+        private readonly int x;
+        private readonly int y;
+
+        public Position(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public IEnumerable<Position> GetNeighbourPositions()
+        {
+            return new[]
+                {
+                    new Position(x - 1, y - 1),
+                    new Position(x, y - 1),
+                    new Position(x + 1, y - 1),
+                    new Position(x - 1, y),
+                    new Position(x + 1, y),
+                    new Position(x - 1, y + 1),
+                    new Position(x, y + 1),
+                    new Position(x + 1, y + 1)
+                };
+        }
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as Position;
+
+            return other != null && (this.x == other.x && this.y == other.y);
+        }
+    }
+
+    public static class Board
+    {
+        public static Position[] Tick(Position[] positions)
+        {
+            if (positions == null || positions.Length == 0)
+                return new Position[0];
+
+            // check the dead neighbours of positions if it needs to spawn new cells
+            var newLivePositions = new List<Position>();
+            foreach (var position in positions)
+            {
+                var deadNeighbours = position.GetNeighbourPositions().Where(p => !positions.Contains(p));
+
+                // spawn a new cell if it has 3 neighbours
+                newLivePositions.AddRange(deadNeighbours.Where(deadNeighbour => deadNeighbour.GetNeighbourPositions().Count(positions.Contains) == 3));
+            }
+
+            // check all the live cells, deci
+            return (from position in positions
+                    let neighbourCount = position.GetNeighbourPositions().Count(positions.Contains)
+                    where Cell.IsAlive(neighbourCount)
+                    select position)
+                    .Concat(newLivePositions)
+                    .ToArray();
+        }
+    }
+
+    public static class Cell
+    {
+        public static bool IsAlive(int noOfNeighbours)
+        {
+            return noOfNeighbours > 1 && noOfNeighbours < 4;
+        }
+    }
+}
---
+++ ClassLibrary1/GoLApproach1.cs
@@ -0,0 +1,130 @@
+﻿namespace ClassLibrary1
+{
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Rules:
+    /// Any live cell with fewer than two live neighbours dies, as if caused by underpopulation.
+    /// Any live cell with more than three live neighbours dies, as if by overcrowding.
+    /// Any live cell with two or three live neighbours lives on to the next generation.
+    /// Any dead cell with exactly three live neighbours becomes a live cell.
+    /// </summary>
+    [TestFixture]
+    public class GoLApproach1
+    {
+        [Test]
+        public void CellDiesInUnderpopulation()
+        {
+            var board = new[]
+                {
+                    new[] { 0, 0, 1 },
+                    new[] { 0, 1, 0 },
+                    new[] { 0, 0, 0 }
+                };
+
+            var expectedBoard = new[]
+                {
+                    new[] { 0, 0, 0 },
+                    new[] { 0, 0, 0 },
+                    new[] { 0, 0, 0 }
+                };
+
+            var newBoard = Tick(board);
+
+            Assert.AreNotEqual(board, newBoard);
+            Assert.That(newBoard, Is.EquivalentTo(expectedBoard));
+        }
+
+        [Test]
+        public void CellDiesInOverpopulation()
+        {
+            var board = new[]
+                {
+                    new[] { 1, 1, 1 },
+                    new[] { 1, 1, 1 },
+                    new[] { 1, 1, 1 }
+                };
+
+            var expectedBoard = new[]
+                {
+                    new[] { 1, 0, 1 },
+                    new[] { 0, 0, 0 },
+                    new[] { 1, 0, 1 }
+                };
+
+            var newBoard = this.Tick(board);
+
+            Assert.That(newBoard, Is.EquivalentTo(expectedBoard));
+        }
+
+        [Test]
+        public void CellBornsWithThreeNeighbours()
+        {
+            var board = new[]
+                {
+                    new[] { 0, 1, 0 },
+                    new[] { 1, 1, 1 },
+                    new[] { 0, 1, 0 }
+                };
+
+            var expectedBoard = new[]
+                {
+                    new[] { 1, 1, 1 },
+                    new[] { 1, 0, 1 },
+                    new[] { 1, 1, 1 }
+                };
+
+            var newBoard = this.Tick(board);
+
+            Assert.That(newBoard, Is.EquivalentTo(expectedBoard));
+        }
+
+        private int[][] Tick(int[][] board)
+        {
+            var newBoard = new int[board.Length][];
+
+            for (int x = 0; x < board.Length; x++)
+            {
+                newBoard[x] = new int[board[0].Length];
+
+                for (int y = 0; y < board[x].Length; y++)
+                {
+                    // calculate neighbours
+                    int neighbours = 0;
+
+                    // north
+                    if (x > 0 && y > 0) neighbours += board[x - 1][y - 1];
+                    if (y > 0) neighbours += board[x][y - 1];
+                    if (x < board.Length-1 && y > 0) neighbours += board[x + 1][y - 1];
+                    
+                    // in-line
+                    if (x > 0) neighbours += board[x - 1][y];
+                    if (x < board.Length-1) neighbours += board[x + 1][y];
+
+                    // south
+                    if (x > 0 && y < board[x].Length-1) neighbours += board[x - 1][y + 1];
+                    if (y < board[x].Length-1) neighbours += board[x][y + 1];
+                    if (x < board.Length-1 && y < board[x].Length-1) neighbours += board[x + 1][y + 1];
+
+                    if (neighbours < 2 || neighbours > 3)
+                    {
+                        // underpopulation or overpopulation
+                        newBoard[x][y] = 0;
+                    }
+                    else if (neighbours == 3)
+                    {
+                        // new born
+                        newBoard[x][y] = 1;
+                    }
+                    else
+                    {
+                        // stays the same
+                        newBoard[x][y] = board[x][y];
+                    }
+                }
+            }
+
+            return newBoard;
+        }
+    }
+}
